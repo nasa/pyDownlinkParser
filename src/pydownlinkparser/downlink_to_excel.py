9af633a5c12aa4c2ef68a5ce2a32631851a50834
nischayn <nischay.nagendra@jpl.nasa.gov>
2023-08-29 09:55:51 -0700

    Resolved comments

--- src/pydownlinkparser/downlink_to_excel.py
+++ src/pydownlinkparser/downlink_to_excel.py
@@ -17,10 +17,8 @@ def get_parser():
 def export_dfs_to_xlsx(dfs, filename1):
     with pd.ExcelWriter(filename1) as writer:
         for apid, df in dfs.items():
-            if apid in apid_packet_structures.apid_packets.keys():
-                name = apid_packet_structures.apid_packets[apid].name
-            else:
-                name = f'{apid}'
+            pkt_struct = apid_packet_structures.apid_packets[apid]
+            name = f'{apid} - ' + pkt_struct.name if hasattr(pkt_struct, "name") else f'{apid}' + pkt_struct.__name__
             df.to_excel(writer, sheet_name=name, index=True)
 
 
