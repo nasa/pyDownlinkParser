bce53ee5c7d20df6c5dea9d73233a72c4ebf7025
thomas loubrieu <thomas.loubrieu@jpl.nasa.gov>
2024-05-02 16:51:17 -0400

    fix field swap in UVS metadata

--- src/ccsds/packets/europa_clipper/uvs/metadata.py
+++ src/ccsds/packets/europa_clipper/uvs/metadata.py
@@ -1,10 +1,12 @@
 """ECM Metadata packet structure."""
+import copy
+
 import ccsdspy
 from ccsds.packets.europa_clipper.common import METADATA_FIELDS
 
 # specifically for UVS metadata packetm the following 2 fields are swapped,
 # unlike the metadata packets for all the other instruments
-uvs_metadata_fields = METADATA_FIELDS
+uvs_metadata_fields = copy.copy(METADATA_FIELDS)
 uvs_metadata_fields[7] = METADATA_FIELDS[8]
 uvs_metadata_fields[8] = METADATA_FIELDS[7]
 
