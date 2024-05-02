c68a4c0de5e06b6299fe4fb9fc8ec84b5709730f
thomas loubrieu <thomas.loubrieu@jpl.nasa.gov>
2024-05-02 15:29:54 -0400

    update reference test because of new metadata packets parsed. remove init in namespace packages.

--- src/ccsds/packets/europa_clipper/__init__.py
+++ src/ccsds/packets/europa_clipper/__init__.py
@@ -3,6 +3,7 @@ import pkgutil
 
 __all__ = []
 for loader, module_name, _ in pkgutil.walk_packages(__path__):
-    __all__.append(module_name)
-    _module = loader.find_module(module_name).load_module(module_name)
-    globals()[module_name] = _module
+    if "test" not in module_name:
+        __all__.append(module_name)
+        _module = loader.find_module(module_name).load_module(module_name)
+        globals()[module_name] = _module
