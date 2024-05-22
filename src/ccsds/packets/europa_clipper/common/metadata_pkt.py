3d38967ea53e5a78609a0f3025bcbdf8f18f5c59
thomas loubrieu <thomas.loubrieu@jpl.nasa.gov>
2024-05-22 16:08:52 -0400

    simplify metadata parsers for europa-clipper

--- src/ccsds/packets/europa_clipper/common/metadata_pkt.py
+++ src/ccsds/packets/europa_clipper/common/metadata_pkt.py
@@ -8,8 +8,7 @@ METADATA_FIELDS = [
     ccsdspy.PacketField(name="AID", bit_length=32, data_type="uint"),
     # ADP Metadata
     ccsdspy.PacketField(name="CTRL", bit_length=5, data_type="uint"),
-    ccsdspy.PacketField(name="ADP_Major_APID", bit_length=5, data_type="uint"),
-    ccsdspy.PacketField(name="ADP_Minor_APID", bit_length=6, data_type="uint"),
+    ccsdspy.PacketField(name="ADP_APID", bit_length=11, data_type="uint"),
     ccsdspy.PacketField(name="SCLK", bit_length=48, data_type="uint"),
     ccsdspy.PacketField(name="Source_Sequence_Count", bit_length=14, data_type="uint"),
     ccsdspy.PacketField(name="Spare", bit_length=2, data_type="uint"),
